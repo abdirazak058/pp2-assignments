60740c69fe9d085cef615c31a6ebb312d356a294
abdirazak058 <aibar08.08@mail.ru>
2026-02-02 16:50:42 +0500

    Labka_3

---
+++ Labka_3/302.py
@@ -0,0 +1,11 @@
+def id(num):
+    for i in [2, 3, 5]:
+        while num % i == 0:
+            num //= i
+    return num == 1
+
+n = int(input())
+if id(n):
+    print("Yes")
+else:
+    print("No")
